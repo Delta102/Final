a2803eccf26d74c95539672acbf4b17d6c955a89
Delta102 <alfrefp10001@gmail.com>
2023-07-06 10:03:40 -0500

    Séptimo Commit

--- app/src/main/java/com/upn/Peralta/Rodriguez/CreateCartaActivity.java
+++ app/src/main/java/com/upn/Peralta/Rodriguez/CreateCartaActivity.java
@@ -41,8 +41,14 @@ public class CreateCartaActivity extends AppCompatActivity {
 
         btnOpenGallery.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                handleOpenGallery();
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                    openGallery();
+                else {
+                    Log.i("MAIN_APP", "NO HAY PERMISOS");
+                    String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE};
+                    requestPermissions(permissions, 2000);
+                }
             }
         });
     }
@@ -50,47 +56,27 @@ public class CreateCartaActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == OPEN_GALLERY_REQUEST && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                if (cursor != null) {
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
 
-        if(requestCode == OPEN_GALLERY_REQUEST && resultCode == RESULT_OK) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close(); // close cursor
-
-            Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
-            imgGaleria.setImageBitmap(bitmap);
-        }
-
-    }
-
-    void handleOpenGallery() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            openGallery();
-        } else {
-            Log.i("MAIN_APP", "No hay permisos");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, OPEN_GALLERY_REQUEST);
+                    Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+                    imgGaleria.setImageBitmap(bitmap);
+                }
+            }
         }
     }
 
     private void openGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
-        startActivityForResult(intent, OPEN_GALLERY_REQUEST);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == OPEN_GALLERY_REQUEST) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openGallery();
-            } else {
-                Toast.makeText(this, "No se otorgaron los permisos necesarios.", Toast.LENGTH_SHORT).show();
-            }
-        }
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), OPEN_GALLERY_REQUEST);
     }
 }
