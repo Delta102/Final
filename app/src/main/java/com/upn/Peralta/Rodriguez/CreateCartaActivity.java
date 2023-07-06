1131a6f29628ffddb1fb62d4b8fb633555e01b4a
Delta102 <alfrefp10001@gmail.com>
2023-07-06 09:31:16 -0500

    Sexto commit

--- app/src/main/java/com/upn/Peralta/Rodriguez/CreateCartaActivity.java
+++ app/src/main/java/com/upn/Peralta/Rodriguez/CreateCartaActivity.java
@@ -1,11 +1,33 @@
 package com.upn.Peralta.Rodriguez;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class CreateCartaActivity extends AppCompatActivity {
+
+    private static final int OPEN_GALLERY_REQUEST = 1002;
+    private static final int LOCATION_PERMISSION_REQUEST = 1003;
+    private static final String urlFotoApi = "https://demo-upn.bit2bittest.com/";
+
+    ImageView imgGaleria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +35,62 @@ public class CreateCartaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_carta);
 
         int idDuelista = getIntent().getIntExtra("idDuelista", -1);
-        
+
+        Button btnOpenGallery = findViewById(R.id.btnOpenGallery);
+        imgGaleria = findViewById(R.id.imgGallery);
+
+        btnOpenGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleOpenGallery();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == OPEN_GALLERY_REQUEST && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close(); // close cursor
+
+            Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+            imgGaleria.setImageBitmap(bitmap);
+        }
+
+    }
+
+    void handleOpenGallery() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            openGallery();
+        } else {
+            Log.i("MAIN_APP", "No hay permisos");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, OPEN_GALLERY_REQUEST);
+        }
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, OPEN_GALLERY_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == OPEN_GALLERY_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            } else {
+                Toast.makeText(this, "No se otorgaron los permisos necesarios.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
